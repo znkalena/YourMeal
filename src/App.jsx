1098ba412983f380b408bd2fee29875fe15dfe33
znkalena <elze66@gmail.com>
2023-02-22 23:05:07 +0300

    add container and header

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Container } from "./components/container/container";
+import { Header } from "./components/header/header";
+
+export const App = () => {
+  return (
+    <>
+    <Header/>  
+    <main>
+<nav>
+  <Container maks = 'leskin' className = "navigation__container" />
+</nav>
+<section></section>
+    </main>
+    <footer></footer>
+    </>
+    
+  )   
+};
+
+
